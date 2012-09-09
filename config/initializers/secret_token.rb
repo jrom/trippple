29a71eb7c0aefbf531e0b9355f9fb3f0f96d5af2
Jordi Romero <jordi@jrom.net>
2012-09-09 19:25:59 +0200

    empty rails 3.2.8 app using thin and quiet_assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trippple::Application.config.secret_token = '74f233fde8379aece9b82be06f97e8f1d84ae1770a4abd6a45cabface1a8996f2888f089e11fa714c30406d4cc77e6a95b34cc45addd1513d27ee02143390dbe'
