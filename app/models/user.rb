20d1cbc83916a0c60917d05ad578eed39c7aa9a6
Roger Campos <roger@itnig.net>
2012-09-12 16:52:25 +0200

    - Add name to users - Example of migration to add the name column - Defined special method "to_s" for the user model, returns the name - Generated the devise views, override the sign up form to ask for the name
    Conflicts:
    
    	db/schema.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+
+  validates_presence_of :name
+
+  def to_s
+    name
+  end
 end
