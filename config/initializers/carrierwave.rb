2596a0dbbd038440333048fb38ccdb2268908ae4
Jordi Romero <jordi@jrom.net>
2012-09-12 18:35:47 +0200

    remove region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => "AWS",
     :aws_access_key_id => ENV['S3_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
-    :region => 'eu-west-1'
+    :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
   }
   config.fog_directory = ENV['S3_BUCKET']
 end
