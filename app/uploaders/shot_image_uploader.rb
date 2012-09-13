6e4b56ec4f0af0a6e8ae71d7f0ede69605f100b7
Roger Campos <roger@itnig.net>
2012-09-13 17:52:09 +0200

    Use :fog as storage in the final stage

--- app/uploaders/shot_image_uploader.rb
+++ app/uploaders/shot_image_uploader.rb
@@ -7,12 +7,7 @@ class ShotImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provide => "AWS",
+    :provider => "AWS",
     :aws_access_key_id => ENV['S3_ACCESS_KEY_ID'],
     :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY'],
     :region => 'eu-west-1'
